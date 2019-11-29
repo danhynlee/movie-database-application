251e896fde0b3f726426007ffa19cb568a2bf702
Daniel Lee <danhynlee@Daniels-MBP.home>
2019-11-28 22:30:08 -0500

    removed alter tables script

--- sql/4400alter_tables.sql
+++
@@ -1,8 +0,0 @@
-alter table team55.creditcard
-modify column CreditCardnum varchar(16);
-
-alter table team55.used
-modify column CCnum varchar(16);
-
-alter table team55.theater
-modify column Name VARCHAR(50) NOT NULL	;
