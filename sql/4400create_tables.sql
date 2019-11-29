f76197a1cac4004272f2497b00f54956a4600c24
Daniel Lee <danhynlee@Daniels-MBP.home>
2019-11-28 22:27:13 -0500

    reorganized create tables and changed some foreign key issues

--- sql/4400create_tables.sql
+++ sql/4400create_tables.sql
@@ -2,140 +2,139 @@ drop database team55;
 create database team55;
 use team55;
 
+CREATE TABLE Company (
+	comName VARCHAR(50) NOT NULL UNIQUE,
+	PRIMARY KEY (comName)
+);
+CREATE TABLE Movie (
+	movName VARCHAR(50) NOT NULL UNIQUE,
+	movReleaseDate DATE NOT NULL UNIQUE,
+	duration INT NOT NULL,
+	PRIMARY KEY (movName, movReleaseDate)
+);
 CREATE TABLE User (
-	Username VARCHAR(50) NOT NULL UNIQUE,
-	Firstname VARCHAR(50) NOT NULL,
-	Lastname VARCHAR(50) NOT NULL,
-	Password VARCHAR(50) NOT NULL,
-	Status VARCHAR(50) NOT NULL,
+	username VARCHAR(50) NOT NULL UNIQUE,
+    status VARCHAR(50) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+	firstname VARCHAR(50) NOT NULL,
+	lastname VARCHAR(50) NOT NULL,
 	PRIMARY KEY (Username)
 );
 CREATE TABLE Employee (
-	Username VARCHAR(50) NOT NULL,
-	PRIMARY KEY (Username),
-	FOREIGN KEY (Username)
-		REFERENCES User (Username)
-			ON DELETE CASCADE
-			ON UPDATE CASCADE
-);
-CREATE TABLE Admin (
-	Username VARCHAR(50) NOT NULL,
-	PRIMARY KEY (Username),
-	FOREIGN KEY (Username)
-		REFERENCES User (Username)
-			ON DELETE CASCADE
-			ON UPDATE CASCADE
-);
-CREATE TABLE Company (
-	Name VARCHAR(50) NOT NULL UNIQUE,
-	PRIMARY KEY (Name)
-);
-CREATE TABLE Manager (
-	Username VARCHAR(50) NOT NULL,
-	MngStreet VARCHAR(50) NOT NULL,
-	MngCity VARCHAR(50) NOT NULL,
-	MngState VARCHAR(50) NOT NULL,
-	MngZipCode VARCHAR(50) NOT NULL,
-	CoName VARCHAR(50) NOT NULL,
-	PRIMARY KEY (Username),
-	FOREIGN KEY (Username)
-		REFERENCES Employee (Username)
-			ON DELETE CASCADE
-			ON UPDATE CASCADE,
-	FOREIGN KEY (CoName)
-		REFERENCES Company (Name)
+	username VARCHAR(50) NOT NULL,
+	PRIMARY KEY (username),
+	FOREIGN KEY (username)
+		REFERENCES User (username)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
 CREATE TABLE Customer (
-	Username VARCHAR(50) NOT NULL,
-	PRIMARY KEY (Username),
-	FOREIGN KEY (Username)
-		REFERENCES User (Username)
+	username VARCHAR(50) NOT NULL,
+	PRIMARY KEY (username),
+	FOREIGN KEY (username)
+		REFERENCES User (username)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
-CREATE TABLE CreditCard (
-	CreditCardNum VARCHAR(16),
-	CUsername VARCHAR(50) NOT NULL,
-	PRIMARY KEY (CreditCardNum),
-	FOREIGN KEY (CUsername)
-		REFERENCES Customer (Username)
+CREATE TABLE Admin (
+	username VARCHAR(50) NOT NULL,
+	PRIMARY KEY (username),
+	FOREIGN KEY (username)
+		REFERENCES Employee (username)
+			ON DELETE CASCADE
+			ON UPDATE CASCADE
+);
+CREATE TABLE Manager (
+	username VARCHAR(50) NOT NULL,
+    comName VARCHAR(50) NOT NULL,
+	manStreet VARCHAR(50) NOT NULL,
+	manCity VARCHAR(50) NOT NULL,
+	manState VARCHAR(50) NOT NULL,
+	manZipcode VARCHAR(50) NOT NULL,
+	PRIMARY KEY (username),
+	FOREIGN KEY (username)
+		REFERENCES Employee (username)
+			ON DELETE CASCADE
+			ON UPDATE CASCADE,
+	FOREIGN KEY (comName)
+		REFERENCES Company (comName)
+			ON DELETE CASCADE
+			ON UPDATE CASCADE
+);
+CREATE TABLE CustomerCreditCard (
+	creditCardNum VARCHAR(16),
+	username VARCHAR(50) NOT NULL,
+	PRIMARY KEY (creditCardNum),
+	FOREIGN KEY (username)
+		REFERENCES Customer (username)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
 CREATE TABLE Theater (
-	Name VARCHAR(50) NOT NULL,
-	CompanyName VARCHAR(50) NOT NULL,
-	ThtrStreet VARCHAR(50) NOT NULL,
-	ThtrCity VARCHAR(50) NOT NULL,
-	ThtrState VARCHAR(50) NOT NULL,
-	ThtrZipcode VARCHAR(50) NOT NULL,
-	Capacity INT NOT NULL,
-	MngUsername VARCHAR(50) NOT NULL,
-	PRIMARY KEY (Name, CompanyName),
-	FOREIGN KEY (CompanyName)
-		REFERENCES Company (Name)
+	thName VARCHAR(50) NOT NULL,
+	comName VARCHAR(50) NOT NULL,
+    capacity INT NOT NULL,
+	thStreet VARCHAR(50) NOT NULL,
+	thCity VARCHAR(50) NOT NULL,
+	thState VARCHAR(50) NOT NULL,
+	thZipcode VARCHAR(50) NOT NULL,
+	manUsername VARCHAR(50) NOT NULL,
+	PRIMARY KEY (thName, comName),
+	FOREIGN KEY (comName)
+		REFERENCES Company (comName)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE,
-	FOREIGN KEY (MngUsername)
-		REFERENCES Manager (Username)
-ON DELETE CASCADE
+	FOREIGN KEY (manUsername)
+		REFERENCES Manager (username)
+			ON DELETE CASCADE
 			ON UPDATE CASCADE
-);
-CREATE TABLE Movie (
-	Name VARCHAR(50) NOT NULL UNIQUE,
-	ReleaseDate DATE NOT NULL UNIQUE,
-	Duration INT NOT NULL,
-	PRIMARY KEY (Name, ReleaseDate)
 );
 CREATE TABLE MoviePlay (
-	PlayDate DATE NOT NULL UNIQUE,
-	MovieName VARCHAR(50) NOT NULL,
-	MovieReleaseDate DATE NOT NULL,
-	TheaterName VARCHAR(50) NOT NULL,
-	CoName VARCHAR(50) NOT NULL,
-	PRIMARY KEY (PlayDate, MovieName, MovieReleaseDate,TheaterName,CoName),
-	FOREIGN KEY (MovieName,MovieReleaseDate)
-		REFERENCES Movie (Name,ReleaseDate)
+	thName VARCHAR(50) NOT NULL,
+    comName VARCHAR(50) NOT NULL,
+	movName VARCHAR(50) NOT NULL,
+	movReleaseDate DATE NOT NULL,
+    movPlayDate DATE NOT NULL UNIQUE,
+	PRIMARY KEY (thName, comName, movName, movReleaseDate, movPlayDate),
+    FOREIGN KEY (thName, comName)
+		REFERENCES Theater (thName, comName)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE,
-	FOREIGN KEY (TheaterName, CoName)
-		REFERENCES Theater(Name, CompanyName)
+	FOREIGN KEY (movName, movReleaseDate)
+		REFERENCES Movie (movName, movReleaseDate)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
-
-CREATE TABLE Visit (
-	ID VARCHAR(50) NOT NULL UNIQUE,
-	Date DATE NOT NULL,
-	Username VARCHAR(50) NOT NULL,
-	TheaterName VARCHAR(50) NOT NULL,
-	CoName VARCHAR(50) NOT NULL,
-	PRIMARY KEY (ID),
-	FOREIGN KEY (Username)
-		REFERENCES User(Username)
+CREATE TABLE VisitUserTheater (
+	visitID VARCHAR(50) NOT NULL UNIQUE,
+	username VARCHAR(50) NOT NULL,
+	thName VARCHAR(50) NOT NULL,
+	comName VARCHAR(50) NOT NULL,
+    visitDate DATE NOT NULL,
+	PRIMARY KEY (visitID),
+	FOREIGN KEY (username)
+		REFERENCES User (username)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE,
-	FOREIGN KEY (TheaterName, CoName)
-		REFERENCES Theater (Name, CompanyName)
+	FOREIGN KEY (thName, comName)
+		REFERENCES Theater (thName, comName)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
-CREATE TABLE Used (
-	CCNum VARCHAR(16),
-	MoviePlayDate DATE NOT NULL,
-	MovieName VARCHAR(50) NOT NULL,
-	MovieReleaseDate DATE NOT NULL,
-	TheaterName VARCHAR(50) NOT NULL,
-	CoName VARCHAR(50) NOT NULL,
-	PRIMARY KEY (CCNum, MoviePlayDate, MovieName, MovieReleaseDate,TheaterName,CoName),
-	FOREIGN KEY (CCNum)
-		REFERENCES CreditCard (CreditCardNum)
+CREATE TABLE CustomerViewMovie (
+	creditCardNum VARCHAR(16),
+    thName VARCHAR(50) NOT NULL,
+    comName VARCHAR(50) NOT NULL,
+	movName VARCHAR(50) NOT NULL,
+	movReleaseDate DATE NOT NULL,
+    movPlayDate DATE NOT NULL,
+	PRIMARY KEY (creditCardNum, thName, comName, movName, movReleaseDate, movPlayDate),
+	FOREIGN KEY (creditCardNum)
+		REFERENCES CustomerCreditCard (creditCardNum)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE,
-	FOREIGN KEY (MoviePlayDate, MovieName, MovieReleaseDate,TheaterName,CoName)
-		REFERENCES MoviePlay (PlayDate, MovieName, MovieReleaseDate,TheaterName,CoName)
+	FOREIGN KEY (thName, comName, movName, movReleaseDate, movPlayDate)
+		REFERENCES MoviePlay (thName, comName, movName, movReleaseDate, movPlayDate)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
