0f85e9c24201116a515db72e8ceddcc60e53f7e9
suebyeon <53916951+suebyeon@users.noreply.github.com>
2019-11-30 21:00:14 -0500

    Update team55_user.sql

--- sql/team55_user.sql
+++ sql/team55_user.sql
@@ -27,7 +27,7 @@ CREATE TABLE `User` (
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `password` varchar(200) NOT NULL,
-  `status` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL DEFAULT("Pending"),
   PRIMARY KEY (`username`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
